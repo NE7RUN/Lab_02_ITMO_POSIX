0010063215df2f9f1d3771ef252f842ba9dac218
Oleg Doronin <dorooleg@niuitmo.ru>
2021-01-11 20:24:07 +0300

    main.cpp has been fixed

--- main.cpp
+++ main.cpp
@@ -1,5 +1,41 @@
-all:
-	$(CXX) *.cpp -std=c++17 -g -O3 -Werror -Wall -Wextra -pthread -pedantic -o posix
-
-clean:
-	rm -f posix
+#include <iostream>
+#include <pthread.h>
+ 
+void* producer_routine(void* arg) {
+  // Wait for consumer to start
+ 
+  // Read data, loop through each value and update the value, notify consumer, wait for consumer to process
+  return nullptr;
+}
+ 
+void* consumer_routine(void* arg) {
+  // notify about start
+  // for every update issued by producer, read the value and add to sum
+  // return pointer to result (for particular consumer)
+  return nullptr;
+}
+ 
+void* consumer_interruptor_routine(void* arg) {
+  // wait for consumers to start
+ 
+  // interrupt random consumer while producer is running                                          
+  return nullptr;
+}
+ 
+int run_threads() {
+  // start N threads and wait until they're done
+  // return aggregated sum of values
+ 
+  return 0;
+}
+ 
+int get_tid() {
+  // 1 to 3+N thread ID
+ 
+  return 0;
+}
+ 
+int main() {
+    std::cout << run_threads() << std::endl;
+    return 0;
+}
