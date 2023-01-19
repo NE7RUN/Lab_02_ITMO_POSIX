5bef59d5f903c3058217640375bcaaf4ea4abf46
Вячеслав Ткаченко <koollt@niuitmo.ru>
2023-01-19 10:19:13 +0000

    Update producer_consumer.h после тестов на Ubuntu 18.04

--- producer_consumer.h
+++ producer_consumer.h
@@ -1,24 +1,47 @@
 #ifndef PRODUCERCONSUMER
+
 #define PRODUCERCONSUMER
+
 #include <pthread.h>
+
+
 
 int run_threads(int, int, bool);
 
+
+
 struct producerInfo {
+
   int* shared;
+
 };
 
-struct interrupterInfo {
+
+
+struct interruptorInfo {
+
   int workingConsumersAmount;
+
   pthread_t* consumers;
+
 };
+
+
 
 struct consumerInfo {
+
   int* shared;
+
   int sleepTime;
+
   int* global;
-  consumerInfo(int* shared, int sleepingTime, int* global)
-      : shared(shared), sleepingTime(sleepingTime), global(global){};
+
+  consumerInfo(int* shared, int sleepTime, int* global)
+
+      : shared(shared), sleepTime(sleepTime), global(global){};
+
 };
+
+
 
 #endif
