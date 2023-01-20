9c47c25dffde5185d5c41bc9f6a83c901966fda9
Vyacheslav Tkachenko <slavik199916@gmail.com>
2023-01-20 15:56:05 +0300

    clang-tidy???????

--- producer_consumer.h
+++ producer_consumer.h
@@ -4,32 +4,19 @@
 
 #include <pthread.h>
 
-
-
 int run_threads(int, int, bool);
 
-
-
 struct producerInfo {
-
   int* shared;
-
 };
 
-
-
 struct interruptorInfo {
-
   int workingConsumersAmount;
 
   pthread_t* consumers;
-
 };
 
-
-
 struct consumerInfo {
-
   int* shared;
 
   int sleepTime;
@@ -39,9 +26,6 @@ struct consumerInfo {
   consumerInfo(int* shared, int sleepTime, int* global)
 
       : shared(shared), sleepTime(sleepTime), global(global){};
-
 };
-
-
 
 #endif
