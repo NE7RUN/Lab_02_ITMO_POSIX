d0e3f9a8947b5734b8dfde08dc213da768f03ac6
Вячеслав Ткаченко <slavik199916@gmail.com>
2023-01-23 22:46:10 +0300

    test

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,10 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 #include <producer_consumer.h>
+#include <iostream>
 
-TEST_CASE("just_example") { CHECK(4 == 4); }
+TEST_CASE("test" * doctest::timeout(1)) {
+  std::istringstream in("1 2 3 4 5");
+  std::cin.rdbuf(in.rdbuf());
+  CHECK(15 == run_threads(1, 0, false));
+}
